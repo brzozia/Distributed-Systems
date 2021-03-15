8e486a17085a555c123e323bd5c5359b7d20f67e
Natalia <natib13@gmail.com>
2021-03-15 05:39:06 +0100

    multi works sh

--- 1_sockets/python_tcp_client_chat.py
+++ 1_sockets/python_tcp_client_chat.py
@@ -1,50 +1,121 @@
 import socket
+import struct
 import time
 import os
 import threading
 
-def receive_fun():
-    buff = client.recv(buff_size)
-    
+def read_msg(buff):
+    sid = buff[-2:]     #get id
+    msg = buff[:-2]     #get msg
+    return int.from_bytes(sid, 'little'), str(msg, 'utf-8')
+
+
+def tcp_receive_fun():
+    buff = socket_tcp.recv(buff_size)
     while buff:
-        sid = buff[-2:]     #get id
-        msg = buff[:-2]     #get msg
-        print("(" + str(int.from_bytes(sid, 'little')) + ") "  + str(msg, 'utf-8') )
-        buff = client.recv(buff_size)
+        sid, msg = read_msg(buff)
+        print('(', sid, ') ', msg)
+        buff = socket_tcp.recv(buff_size)
 
     # if server has closed connection
-    print("lost connection with server")
+    print('lost connection with server')
     closing_connection()
 
 
-def send_fun(msg):
+def udp_m_receive_fun(socket):
+    try:
+        while True:
+            buff, _addr = socket.recvfrom(buff_size)
+            sid, msg = read_msg(buff)
+            print('(', sid, ') ', msg)
+    except:
+        print("exception occured")
+    finally:
+        socket.close()
+
+
+# def m_receive_fun():
+#     while True:
+#         buff, _uaddr = socket_udp.recvfrom(buff_size)
+#         sid, msg = read_msg(buff)
+#         print('(', sid, ') ', msg)
+    
+
+def send_fun():
+    msg = input() 
     while msg!='close':
-        client.send(bytes(msg,'utf-8')+sname)    #send msg with sockets id
+        if msg=='U':    #send msg using UDP
+            msg = input()
+            msg2 = """
+                        .-.'  '.-.
+                    .-(   \  /   )-.
+                    /   '..oOOo..'   |
+            ,       \.--.oOOOOOOo.--./
+            |\  ,   (   :oOOOOOOo:   )
+            _\.\/|   /'--'oOOOOOOo'--'|
+            '-.. ;/| \   .''oOOo''.   /
+            .--`'. :/|'-(   /  \   )-'
+            '--. `. / //'-'.__.'-;
+            `'-,_';//      ,  /|
+                    '((       |\/./_
+                    \\  . |\; ..-'
+                    \\ |\: .'`--.
+                        \\, .' .--'
+                        ))'_,-'`
+                jgs  //-'
+                    // 
+                    //
+                    |/
+            """
+            socket_udp.sendto(bytes(msg,'utf-8'), (serverIP, serverPort))
+        elif msg == 'M':
+            msg = input()
+            socket_m.sendto(bytes(msg,'utf-8')+mname, (multiIP, multiPort))
+        else:
+            socket_tcp.send(bytes(msg,'utf-8')+sname)    #send msg with sockets id
         msg = input()   #enter new msg
-    client.shutdown(2)
-    closing_connection()
+    socket_tcp.shutdown(2)
     
 
 def closing_connection():
-    client.close()
+    socket_tcp.close()
+    socket_udp.close()
     os._exit(0)
 
 
-if __name__ == "__main__":
-    serverIP = "127.0.0.1"
+if __name__ == '__main__':
+    serverIP = '127.0.0.1'
+    multiIP = '225.0.0.0'
     serverPort = 9009
-    msg = "witaj!"
+    multiPort = 9010
     buff_size = 1024
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((serverIP,serverPort))
+    socket_m = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # socket_m.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+    socket_m.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_m.bind(('', multiPort))
+    group = socket.inet_aton(multiIP)
+    mreq = struct.pack('4sL', group, socket.INADDR_ANY) 
+    socket_m.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    mname = multiPort.to_bytes(2,'little')
 
-    sname = client.getsockname()[1].to_bytes(2,'little')    #get socket id - port number
-    print('PYTHON TCP CLIENT id: ' + str(int.from_bytes(sname, 'little')))
+    socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_tcp.connect((serverIP,serverPort))
+    port = socket_tcp.getsockname()[1]
 
-    client_thread = threading.Thread(target=receive_fun, daemon=True)   #new thread to receive msgs
-    client_thread.start()
+    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_udp.bind(('', port))
 
-    send_fun(msg)
+    sname = port.to_bytes(2,'little')    #get socket id - port number
+    print('PYTHON CLIENT id: ' + str(int.from_bytes(sname, 'little')))
+
+    tcp_client_thread = threading.Thread(target=tcp_receive_fun, daemon=True)   #new thread to receive msgs
+    tcp_client_thread.start()
+    udp_client_thread = threading.Thread(target=udp_m_receive_fun, args=(socket_udp,), daemon=True)   #new thread to receive msgs
+    udp_client_thread.start()
+    m_client_thread = threading.Thread(target=udp_m_receive_fun, args=(socket_m,), daemon=True)   #new thread to receive msgs
+    m_client_thread.start()
+
+    send_fun()
         
 
 
--- 1_sockets/python_tcp_server_chat.py
+++ 1_sockets/python_tcp_server_chat.py
@@ -4,54 +4,65 @@ import threading
 
 
 def client_fun(client, addr):
-    buff = client.recv(buff_size)
-    
-    while buff:
-        sid = buff[-2:]     #get id
-        msg = buff[:-2]     #get msg
-        print("server received msg: " + "(" + str(int.from_bytes(sid, 'little')) + ") " + str(msg, 'utf-8'))
-
-        for key in threads:
-            if key != addr:
-                threads[key][0].send(buff)
         buff = client.recv(buff_size)
+        while buff:
+            sid = buff[-2:]     #get id
+            msg = buff[:-2]     #get msg
+            print('server received msg TCP: ' + '(' + str(int.from_bytes(sid, 'little')) + ') ' + str(msg, 'utf-8'))
+            for key in sockets:
+                if key != addr:
+                    sockets[key].send(buff)
+            buff = client.recv(buff_size)
 
-    print("closing connection with ", addr[1])
-    if inp != 'close':
-        threads.pop(addr)   # remove socket from dict
-    client.close()  
-    sys.exit()  # close this thread
+        print('closing connection with ', addr[1])
+        if inp != 'close':
+            sockets.pop(addr)   # remove socket from dict
+        client.close()  
+        sys.exit()  # close this thread
 
 
-def comm_fun():
+def tcp_comm_fun():
     while True:
-        client, addr = server_socket.accept()
+        client, addr = server_socket_tcp.accept()
         client_thread = threading.Thread(target=client_fun, args=(client,addr,), daemon=True)
-        threads[addr] = (client,client_thread)
+        sockets[addr] = client
         client_thread.start()
-        print("thread no", addr[1], "connected")
-    
+        # print('thread no', addr[1], 'connected')
 
-if __name__ == "__main__":
-    serverIP = "127.0.0.1"
+
+def udp_comm_fun(client):
+    while True:
+        buff, uaddr = client.recvfrom(buff_size)
+        print('server received msg UDP: ' + '(' + str(uaddr[1]) + ') ' + str(buff, 'utf-8'))
+        for key in sockets:
+            if key != uaddr:
+                client.sendto(buff+uaddr[1].to_bytes(2,'little'), key)
+
+
+if __name__ == '__main__':
+    serverIP = '127.0.0.1'
     serverPort = 9009
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((serverIP, serverPort))
-    server_socket.listen()
+    server_socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket_tcp.bind((serverIP, serverPort))
+    server_socket_tcp.listen()
+    server_socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket_udp.bind(('', serverPort))
     buff_size = 1024
-    threads = {}
+    sockets = {}
     inp = ''
 
-    comm_thread = threading.Thread(target=comm_fun, daemon=True)
-    comm_thread.start()
+    tcp_comm_thread = threading.Thread(target=tcp_comm_fun, daemon=True)
+    tcp_comm_thread.start()
+    udp_comm_thread = threading.Thread(target=udp_comm_fun, args=(server_socket_udp,), daemon=True)
+    udp_comm_thread.start()
 
-    print('PYTHON TCP SERVER')
+    print('PYTHON SERVER')
 
     while inp != 'close':
         inp = input()
 
-    print("closing all connections")
-    for key in threads:
-        threads[key][0].shutdown(2)
+    print('closing all connections')
+    for key in sockets:
+        sockets[key].shutdown(2)
 
 
